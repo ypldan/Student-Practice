88009a75534a31a20ab664b95b65604a2a2494f0
Daniil Yarmalkevich <ydpl1998@gmail.com>
2018-04-08 17:53:21 +0300

    fixed bugs

--- UI/loadDOM.js
+++ UI/loadDOM.js
@@ -1,11 +1,11 @@
 loadDOM = function () {
     myDOM.addFormsListeners();
-    myDOM.setUserConfiguration();
     myDOM.createFilter();
     myDOM.firstPostsLoad();
+    myDOM.setUserConfiguration();
     myDOM.createPost({
         description: 'My first new Post!!!',
-        hashtags: ['#hey', '#physics-cool'],
+        hashtags: ['#hey', '#physics_cool'],
         photoLink: 'https://static.appvn.com/a/uploads/thumbnails/112015/mr-square_icon.png'
     });
     myDOM.editPhotoPost(19, {description: 'It has been edited!!!'});
