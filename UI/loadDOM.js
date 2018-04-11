7350b8ae04586a77749ec1ca7ab95d00437857a3
Daniil Yarmalkevich <ydpl1998@gmail.com>
2018-04-11 17:12:09 +0300

    added loading from local storage

--- UI/loadDOM.js
+++ UI/loadDOM.js
@@ -1,13 +1,22 @@
 loadDOM = function () {
     myDOM.addFormsAndFilterListeners();
     myDOM.loadAuthors();
-    myDOM.firstPostsLoad();
-    myDOM.setUserConfiguration();
-    myDOM.createPost({
-        description: 'My first new Post!!!',
-        hashtags: new Set(['#hey', '#physics_cool']),
-        photoLink: 'https://static.appvn.com/a/uploads/thumbnails/112015/mr-square_icon.png'
-    });
-    myDOM.editPhotoPost(19, {description: 'It has been edited!!!'});
+    if (localStorage.getItem("firstLoad")!==undefined && localStorage.getItem("firstLoad")!=="null") {
+        myDOM.setUser(myLocalStorage.getUser());
+        myDOM.setUserConfiguration();
+        myDOM.setAndShowCurrentPosts(myLocalStorage.getCurrentPosts());
+        myDOM.setFilterFromLocalStorage(myLocalStorage.getFilter());
+        MyPortal.setAllPosts(myLocalStorage.getAllPosts());
+    } else {
+        myDOM.firstPostsLoad();
+        myDOM.setUserConfiguration();
+        myDOM.createPost({
+            description: 'My first new Post!!!',
+            hashtags: new Set(['#hey', '#physics_cool']),
+            photoLink: 'https://static.appvn.com/a/uploads/thumbnails/112015/mr-square_icon.png'
+        });
+        myDOM.editPhotoPost(19, {description: 'It has been edited!!!'});
+        localStorage.setItem("firstLoad", "loaded");
+    }
 };
 document.addEventListener("DOMContentLoaded", loadDOM);
